58aa84a6d2edf7fcacc1ee25b08346ea015c1d27
adamgroban <grobanadam@gmail.com>
2026-02-02 23:17:10 +0000

    Deploying to gh-pages from @ adamgroban/adamgroban.github.io@fa9d33dde9d80d18a5f5c2b44ea9f2f2be959513 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -466,11 +466,21 @@ ninja.data = [{
           description: "another without an image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-stable-stove",
-          title: 'Stable Stove',
-          description: "A new type of portable camping stove that reduces tipping hazard while in mountainous terrain",
+            },},{id: "projects-project-5",
+          title: 'project 5',
+          description: "a project with a background image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/stable-stove/";
+              window.location.href = "/projects/5_project/";
+            },},{id: "projects-project-6",
+          title: 'project 6',
+          description: "a project with no image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/6_project/";
+            },},{id: "projects-project-7",
+          title: 'project 7',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/7_project/";
             },},{
         id: 'social-email',
         title: 'email',
