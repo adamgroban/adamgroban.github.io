d8c1a072d53e0257565aae6467fd055f4c5761ae
adamgroban <grobanadam@gmail.com>
2026-02-04 05:43:44 +0000

    Deploying to gh-pages from @ adamgroban/adamgroban.github.io@b792ff8ccbd9bfa0a3b10d1d9542c45e10b274fc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,7 +10,7 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-projects",
-          title: "projects",
+          title: "Projects",
           description: "Relevant projects",
           section: "Navigation",
           handler: () => {
